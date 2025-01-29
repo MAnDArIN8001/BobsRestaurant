a4d1546f7de8e49ba6f15584a6898fed7e60ee4d
Artem Stepanenko <alexplay.stepanenko@gmail.com>
2025-01-29 21:21:56 +0300

    feat: player base

---
+++ Assets/Scripts/Setups/Bob/BobSetup.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Setups.Bob
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class BobSetup : ScriptableObject
+    {
+        
+    }
+}
